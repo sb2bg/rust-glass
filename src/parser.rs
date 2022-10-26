b6693807c7b824d86c7bc6069a9bc67ff91ea8aa
sb2bg <sullivanbognar@gmail.com>
2022-10-26 15:48:53 -0500

    Template some grammar rules

--- src/parser.rs
+++ src/parser.rs
@@ -8,24 +8,78 @@ use logos::Span;
 pub struct Parser {
     tokens: VecDeque<(Token, Span)>,
     source: String,
+    last_token: Option<(Token, Span)>,
+    filename: String,
 }
 
+type ParseResult = Result<Node, GlassError>;
+
 impl Parser {
-    pub fn new(tokens: VecDeque<(Token, Span)>, source: String) -> Self {
-        Self { tokens, source }
+    pub fn new(tokens: VecDeque<(Token, Span)>, source: String, filename: String) -> Self {
+        Self {
+            tokens,
+            source,
+            last_token: None,
+            filename,
+        }
     }
 
-    pub fn parse(&mut self) -> Result<(), GlassError> {
-        while let Some(token) = self.next()? {}
-
-        Ok(())
+    pub fn parse(&mut self) -> ParseResult {
+        loop {
+            println!("{:?}", self.parse_atom()?);
+        }
     }
 
-    fn parse_expression(&mut self) -> Result<Node, GlassError> {
-        todo!()
+    fn parse_expression(&mut self) -> ParseResult {
+        todo!("parse_expression")
     }
 
-    fn parse_atom(&mut self) -> Result<Node, GlassError> {
+    fn parse_math_expression(
+        &mut self,
+        a: &mut dyn FnMut() -> ParseResult,
+        types: Vec<Token>,
+    ) -> ParseResult {
+        let mut left = a()?;
+
+        while let Ok(Some((token, _))) = self.next() {
+            if types.contains(&token) {
+                let right = a()?;
+
+                left = Node::BinaryOp {
+                    left: Box::new(left),
+                    op: token,
+                    right: Box::new(right),
+                };
+            }
+        }
+
+        Ok(left)
+    }
+
+    fn parse_arithmetic(&mut self) -> ParseResult {
+        self.parse_math_expression(&mut || self.parse_term(), vec![Token::Plus, Token::Minus])
+    }
+
+    fn parse_term(&mut self) -> ParseResult {
+        self.parse_math_expression(
+            &mut || self.parse_factor(),
+            vec![Token::Star, Token::Slash, Token::Percent],
+        )
+    }
+
+    fn parse_factor(&mut self) -> ParseResult {
+        todo!("parse_factor")
+    }
+
+    fn parse_power(&mut self) -> ParseResult {
+        todo!("parse_power")
+    }
+
+    fn parse_call(&mut self) -> ParseResult {
+        todo!("parse_call")
+    }
+
+    fn parse_atom(&mut self) -> ParseResult {
         let token = self.next()?;
 
         match token {
@@ -37,7 +91,7 @@ impl Parser {
                 self.expect(Token::RParen)?;
                 Ok(node)
             }
-            _ => todo!(),
+            _ => todo!("unimplemented token {:?}", token),
         }
     }
 
@@ -51,14 +105,22 @@ impl Parser {
                 Err(GlassError::UnexpectedToken {
                     expected: token,
                     actual: next_token,
-                    src: self.source.clone(),
+                    src: self.source.clone(), // todo: don't clone
                     span,
                 })
             }
         } else {
             Err(GlassError::UnexpectedEndOfInput {
                 src: self.source.clone(),
-                span: todo!(),
+                span: match &self.last_token {
+                    Some((_, span)) => span.clone(),
+                    None => {
+                        return Err(GlassError::EmptyTokenStream {
+                            src: self.source.clone(),
+                            filename: self.filename.clone(), // todo: don't clone
+                        });
+                    }
+                },
             })
         }
     }
@@ -69,20 +131,20 @@ impl Parser {
                 match token {
                     Token::Error => {
                         return Err(GlassError::UnknownToken {
-                            src: self.source.clone(), // todo - don't clone
+                            src: self.source.clone(), // todo: don't clone
                             span,
                         });
                     }
                     Token::UnclosedString => {
                         return Err(GlassError::UnclosedString {
-                            src: self.source.clone(), // todo - don't clone
+                            src: self.source.clone(), // todo: don't clone
                             span,
                         });
                     }
                     Token::InvalidEscapeSequence => {
                         return Err(GlassError::UnknownEscapeSequence {
                             escape_sequence: self.source[span.start..span.end].to_string(),
-                            src: self.source.clone(), // todo - don't clone
+                            src: self.source.clone(), // todo: don't clone
                             span,
                         });
                     }
