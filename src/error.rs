1d559f388127e21f274398f7a43da2ebff9d130e
sb2bg <sullivanbognar@gmail.com>
2022-11-08 01:56:28 -0600

    Fix affected range span

--- src/error.rs
+++ src/error.rs
@@ -1,17 +1,28 @@
 use crate::Token;
 use git_version::git_version;
-use line_span::find_line_range;
+use line_span::{find_line_end, find_line_start};
 use logos::Span;
+use std::cmp::min;
 use std::fmt::Debug;
 use std::rc::Rc;
 use thiserror::Error;
 
-fn get_token<'a>(source: &'a str, span: &'a Span) -> &'a str {
-    source[span.start..span.end].trim()
+fn get_token<'a>(src: &'a str, span: &'a Span) -> &'a str {
+    src[span.start..span.end].trim()
 }
 
-fn get_line<'a>(source: &'a str, span: &'a Span) -> &'a str {
-    &source[find_line_range(source, span.start)].trim()
+fn get_line<'a>(src: &'a str, filename: &'a str, span: &'a Span) -> String {
+    let start = find_line_start(src, span.start);
+    let end = find_line_end(src, span.start);
+    let line = &src[start..end].trim();
+    let line_num = src[..span.start].lines().count();
+    let affected_range = span.start - start..span.end - start;
+
+    format!(
+        "\n\n\t{line}\n\t{}{}\n[{filename}(Ln:{line_num}, Col:{affected_range:?})]",
+        &" ".repeat(affected_range.start),
+        &"^".repeat(min(affected_range.len(), line.len() - affected_range.start)),
+    )
 }
 
 // todo: instead of passing source to each error, just get the span and pass that to the error handler in main?
@@ -21,24 +32,38 @@ pub enum GlassError {
     #[error("Unknown error '{error_message}'. Please report this bug with the following information: Glass Version = '{}', Git Revision = '{}'", env!("CARGO_PKG_VERSION"), git_version!(fallback = "<unknown>"))]
     UnknownError { error_message: String },
 
+    #[error("File '{filename}' not found")]
+    FileNotFound { filename: Rc<str> },
+
     #[error(
         "Unknown token '{}' encountered at {}",
         get_token(src, span),
-        get_line(src, span)
+        get_line(src, filename, span)
     )]
-    UnknownToken { src: Rc<str>, span: Span },
-
-    #[error("Unclosed string literal starting at {}", get_line(src, span))]
-    UnclosedString { src: Rc<str>, span: Span },
+    UnknownToken {
+        src: Rc<str>,
+        span: Span,
+        filename: Rc<str>,
+    },
 
     #[error(
-        "Unknown escape sequence '{escape_sequence}' at {} {}",
-        get_token(src, span),
-        get_line(src, span)
+        "Unclosed string literal starting at {}",
+        get_line(src, filename, span)
+    )]
+    UnclosedString {
+        src: Rc<str>,
+        span: Span,
+        filename: Rc<str>,
+    },
+
+    #[error(
+        "Unknown escape sequence '{escape_sequence}' at {}",
+        get_line(src, filename, span)
     )]
     UnknownEscapeSequence {
         escape_sequence: String,
         src: Rc<str>,
+        filename: Rc<str>,
         span: Span,
     },
 
@@ -46,25 +71,33 @@ pub enum GlassError {
     #[error(
         "{}",
         if let Some(expected) = expected {
-            format!("Expected '{}' but found '{}' instead at {}", expected.get_rep(), get_token(src, span), get_line(src, span))
+            format!("Expected '{}' but found '{}' instead at {}", expected.get_rep(), get_token(src, span), get_line(src, filename, span))
         } else {
-            format!("Unexpected token '{}' at {}", get_token(src, span), get_line(src, span))
+            format!("Unexpected token '{}' at {}", get_token(src, span), get_line(src, filename, span))
         }
     )]
     UnexpectedToken {
         expected: Option<Token>,
         src: Rc<str>,
+        filename: Rc<str>,
         span: Span,
     },
 
-    #[error("Unexpected end of input in source file '{filename}'")]
+    #[error("Unexpected end of file in source file '{filename}'")]
     UnexpectedEndOfInput { filename: Rc<str> },
 
-    #[error("Cannot use operation '{operation}' on type '{left}' and '{right}'")]
+    #[error(
+        "Cannot use operation '{operation}' on type '{left}' and '{right}' at {}",
+        "get_line(src, filename, span)"
+    )]
     InvalidOperation {
         operation: String,
         left: String,
         right: String,
+        // todo: once we get spans and sources passed to interpreter
+        // src: Rc<str>,
+        // filename: Rc<str>,
+        // span: Span,
     },
 
     #[error("Unary operator '{operation}' cannot be applied to type '{operand}'")]
